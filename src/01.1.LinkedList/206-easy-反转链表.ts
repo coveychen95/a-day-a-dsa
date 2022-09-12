902383761adc99319be0ca56e3a4439b649539b5
kvchen95 <kvchen95@outlook.com>
2022-09-12 15:34:59 +0800

    feat: 1

--- src/01.1.LinkedList/206-easy-反转链表.ts
+++ src/01.1.LinkedList/206-easy-反转链表.ts
@@ -1,11 +1,10 @@
 // 迭代
 function reverseList(head: ListNode | null): ListNode | null {
-  let pre = null
+  let pre: ListNode | null = null
   let curr = head
   while (curr) {
     const next = curr.next
     curr.next = pre
-
     pre = curr
     curr = next
   }
