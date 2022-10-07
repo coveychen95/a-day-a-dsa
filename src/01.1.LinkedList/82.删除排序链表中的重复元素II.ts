65fefdbb3171191c58881201fab6132c3bfe73a4
kvchen95 <kvchen95@outlook.com>
2022-10-07 23:33:52 +0800

    feat: 1

---
+++ src/01.1.LinkedList/82.删除排序链表中的重复元素II.ts
@@ -0,0 +1,17 @@
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head) return head
+  let res = new ListNode(0, head)
+  let curr = res
+  while (curr.next && curr.next.next) {
+    if (curr.next.val === curr.next.next.val) {
+      const x = curr.next.val;
+      while (curr.next && curr.next.val === x) {
+        curr.next = curr.next.next;
+      }
+    } else {
+      curr = curr.next
+    }
+  }
+  return res.next
+};
