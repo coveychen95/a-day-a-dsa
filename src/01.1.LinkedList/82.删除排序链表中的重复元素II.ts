b412797c6abccb17e3255dc9ca29cfef551f71e7
kvchen95 <kvchen95@outlook.com>
2022-11-27 11:39:30 +0800

    feat:

--- src/01.1.LinkedList/82.删除排序链表中的重复元素II.ts
+++ src/01.1.LinkedList/82.删除排序链表中的重复元素II.ts
@@ -1,18 +1,18 @@
 import { ListNode } from "../main";
 
-function deleteDuplicates(head: ListNode | null): ListNode | null {
+export function deleteDuplicates(head: ListNode | null): ListNode | null {
   if (!head) return head
-  let res = new ListNode(0, head)
-  let curr = res
+  let nil = new ListNode(0, head)
+  let curr = nil
   while (curr.next && curr.next.next) {
     if (curr.next.val === curr.next.next.val) {
-      const x = curr.next.val;
-      while (curr.next && curr.next.val === x) {
+      const saveVal = curr.next.val;
+      while (curr.next && curr.next.val === saveVal) {
         curr.next = curr.next.next;
       }
     } else {
       curr = curr.next
     }
   }
-  return res.next
+  return nil.next
 };
