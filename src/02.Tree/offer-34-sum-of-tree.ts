c9b73ecd7da24de72ac1919a62cb7c7a5170a2d6
kvchen95 <kvchen95@outlook.com>
2022-12-26 22:58:26 +0800

    feat:

--- src/02.Tree/offer-34-sum-of-tree.ts
+++ src/02.Tree/offer-34-sum-of-tree.ts
@@ -1,5 +1,9 @@
 import { TreeNode } from "../main";
 
+// DFS
 function pathSum(root: TreeNode | null, target: number): number[][] {
-
+  const res:number[][] = []
+  const path = []
+  
+  return res
 };
