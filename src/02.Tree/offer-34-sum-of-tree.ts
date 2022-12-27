7888f94ac1bcde4cd15584fe1bfa412d8e086922
kvchen95 <kvchen95@outlook.com>
2022-12-27 23:00:57 +0800

    feat: 1

--- src/02.Tree/offer-34-sum-of-tree.ts
+++ src/02.Tree/offer-34-sum-of-tree.ts
@@ -3,15 +3,19 @@ import { TreeNode } from "../main";
 // DFS
 function pathSum(root: TreeNode | null, target: number): number[][] {
   const res:number[][] = []
-  const path = []
+  const path:number[] = []
+  dfs(root, target)
 
-  function dfs(root: TreeNode, target: number): void {
+  function dfs(root: TreeNode | null, target: number): void {
     if (root === null) {
       return
     }
+    path.push(root.val)
     const tar = target - root.val
-    if (root.left) dfs(root.left, tar)
-    if (root.right) dfs(root.right, tar)
+    if (!root.left && !root.right && target === 0) res.push(path)
+    dfs(root.left, tar)
+    dfs(root.right, tar)
+    path.pop()
   }
 
   return res
